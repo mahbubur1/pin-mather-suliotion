409fc5fb59687d3e85db54e61b85629bc8fe0339
mahbubur1 <mdmahbuborr462@gail.com>
2022-02-16 18:55:45 +0600

    all done

--- js/app.js
+++ js/app.js
@@ -8,9 +8,37 @@ function getPin(){
         getPin();
     }
 }
-
-
 function generateButton(){
    const pin = getPin();
    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click',function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('numbers-display');
+    if(isNaN(number)){
+        
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+    else{
+    const perviousNumber = calcInput.value;
+    const newNumber = perviousNumber + number;
+    calcInput.value = newNumber;
+    }
+});
+function verifyPin(){
+   const pinGenerate = document.getElementById('display-pin').value;
+   const numbersDisplay = document.getElementById('numbers-display').value;
+   const notifySuccess = document.getElementById('notify-success');
+   const notifyFail= document.getElementById('notify-fail');
+   if(pinGenerate == numbersDisplay){
+      notifyFail.style.display = 'none';
+      notifySuccess.style.display = 'block';
+   }
+   else{
+     notifySuccess.style.display = 'none';
+     notifyFail.style.display = 'block';
+   }
 }
